1678b5cda735c7b7660d9c5d3842410081a12b01
ponce <aliloko@gmail.com>
2014-07-31 23:05:16 +0200

    remove files

--- main.d
+++ main.d
@@ -11,8 +11,6 @@ import gfm.math.vector;
 import aosmap;
 import area;
 import block;
-import tower;
-import terrain;
 
 enum MAJOR_VERSION = 0;
 enum MINOR_VERSION = 2;
@@ -82,36 +80,7 @@ void main(string[] argv)
     
     writefln("*** Generating seed %s...", seed);
 
-    int floors = dice(rng, 7, 11);
-    int cellsX = dice(rng, 21, 41);
-    int cellsY = dice(rng, 21, 41);
 
-    makeTerrain(rng, map);
-
-    vec3i cellSize = vec3i(4, 4, 6);
-    
-    vec3i numCells = vec3i(cellsX, cellsY, floors);
-    vec3i dimensions = numCells * cellSize + 1;
-    vec3i towerPos = vec3i(254 - dimensions.x/2, 254 - dimensions.y/2, 1);    
-    box3i blueSpawnArea;
-    box3i greenSpawnArea;
-
-    writefln("- cell size is %s", pythonTuple(cellSize));
-    writefln("- num cells is %s", pythonTuple(numCells));
-
-    makeTower(rng, map, towerPos, numCells, cellSize, blueSpawnArea, greenSpawnArea);
-
-    debug {} else
-    {
-        writefln("*** Color bleeding...");
-        map.colorBleed();
-
-        writefln("*** Compute omnidirectional Ambient Occlusion...");
-        map.betterAO();
-
-        writefln("*** Reverse client Ambient Occlusion...");
-        map.reverseClientAO();
-    }
 
     writefln("*** Saving map to %s...", outputFileVXL);
     map.writeMap(outputFileVXL);
@@ -122,27 +91,14 @@ void main(string[] argv)
     {
         auto f = File(outputFileTXT, "w"); // open for writing
 
-        f.writefln("name = 'Labyrinth Tower'");
+        f.writefln("name = 'Random dungeon'");
         f.writefln("version = '%s.%s'", MAJOR_VERSION, MINOR_VERSION);
-        f.writefln("author = 'vxlgen'");
-        f.writefln("description = ('Labyrinth generated map')");
+        f.writefln("author = 'vxlgen ~dungeon'");
+        f.writefln("description = ('Dungeon generated map')");
         f.writefln("seed = %s", seed);
 
         f.writefln("extensions = {");
 
-        f.writefln("  'tower_position': %s,", pythonTuple(towerPos) );
-        f.writefln("  'tower_cells': %s,", pythonTuple(numCells) );
-        f.writefln("  'cell_size': %s,", pythonTuple(cellSize) );
-        vec3f blueSpawnPos = cast(vec3f)(blueSpawnArea.min + blueSpawnArea.max) / 2.0f;
-        vec3f greenSpawnPos = cast(vec3f)(greenSpawnArea.min + greenSpawnArea.max) / 2.0f;
-        blueSpawnPos.z = 63 - 7;
-        greenSpawnPos.z = 63 - 7;
-
-        f.writefln("  'blue_base_coord': %s,", pythonTuple(blueSpawnPos));
-        f.writefln("  'green_base_coord': %s", pythonTuple(greenSpawnPos));
-
-        //f.writefln("  'blue_base_coord': %s,", pythonTuple(vec3f(254.5f - 55.0f, 254.5f, 63 - 7)));
-        //f.writefln("  'green_base_coord': %s", pythonTuple(vec3f(254.5f + 55.0f, 254.5f, 63 - 7)));
 
         f.writefln("}");
         
@@ -150,25 +106,6 @@ void main(string[] argv)
     }
     writeTXT(outputFileTXT, seed);
 
-}
-
-void makeTower(ref Random rng, AOSMap map, vec3i towerPos, vec3i numCells, vec3i cellSize, out box3i blueSpawnArea, out box3i greenSpawnArea)
-{
-    assert(cellSize == vec3i(4, 4, 6)); // TODO other cell size?
-    writefln("*** Build tower...");
-      
-    auto tower = new Tower(towerPos, numCells);
-    tower.buildBlocks(rng, map);
-
-    blueSpawnArea = box3i(towerPos + tower.blueEntrance.min * cellSize, towerPos + tower.blueEntrance.max * cellSize);
-    greenSpawnArea = box3i(towerPos + tower.greenEntrance.min * cellSize, towerPos + tower.greenEntrance.max * cellSize);
-}
-
-void makeTerrain(ref Random rng, AOSMap map)
-{
-    writefln("*** Generate terrain...");
-    auto terrain = new Terrain(vec2i(512, 512), rng);
-    terrain.buildBlocks(rng, map);
 }
 
 string pythonTuple(vec3i v)
