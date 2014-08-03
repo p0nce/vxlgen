6ac2ecec87da2ef7a92fefdc18fc497e14f652a9
ponce <aliloko@gmail.com>
2014-08-03 23:42:44 +0200

    Generates a map with doodads voxel assets.

--- doodad.d
+++ doodad.d
@@ -4,6 +4,7 @@ import std.file;
 import gfm.math;
 import colorize;
 import voxd;
+import aosmap;
 
 
 class Doodad
@@ -32,16 +33,76 @@ Doodad[] loadAllDoodads()
     return result;
 }
 
+alias Direction = int;
+
+void direction2vector(Direction dir, out mat2x2i m)
+{
+    switch(dir)
+    {
+        case 0: 
+            m = mat2i(1, 0, 
+                      0, 1); 
+            break;
+        case 1: 
+            m = mat2i(0, 1, 
+                      -1, 0); 
+            break;
+        case 2:
+            m = mat2i(-1, 0, 
+                      0, -1); 
+            break;
+        case 3:
+            m = mat2i(0, -1, 
+                      1, 0); 
+            break;
+        default:
+            assert(0);
+    }
+}
+
 class DoodadInstance
 {
 public:
-    this(Doodad doodad, vec3i pos)
+    this(Doodad doodad, vec3i pos, Direction dir)
     {
         _pos = pos;
         _doodad = doodad;
+        _dir = dir;
+    }
+
+    bool isVisible(VoxColor c)
+    {
+        if (c.a == 0)
+            return false;
+
+        if (c.r == 0 && c.g == 255 && c.b == 0)
+            return false;
+
+        return true;
+    }
+
+    void render(AOSMap map)
+    {
+        mat2i mrot;
+        direction2vector(_dir, mrot);
+        int width = _doodad._vox.width;
+        int height = _doodad._vox.height;
+        int depth = _doodad._vox.depth;
+        for (int z = 0; z < depth; ++z)
+            for (int y = 0; y < height; ++y)
+                for (int x = 0; x < width; ++x)
+                {
+                    vec2i disp = mrot * vec2i(x, y);
+                    VoxColor c = _doodad._vox.voxel(x, y, z);
+                    if (isVisible(c))
+                        map.block(_pos.x + disp.x, _pos.y + disp.y, _pos.z + z).seti(c.r, c.g, c.b);
+                    else
+                        map.block(_pos.x + disp.x, _pos.y + disp.y, _pos.z + z).empty();
+                }
     }
 
 private:
     Doodad _doodad;
     vec3i _pos;
+    Direction _dir;
 }
--- dungeon.d
+++ dungeon.d
@@ -13,83 +13,36 @@ class Dungeon
     Room[] rooms;
     Doodad[] doodads;
 
+    DoodadInstance[] objects;
+
     this(ref RNG rng)
     {
         doodads = loadAllDoodads();
         cwritefln( color("*** Loaded %s doodads", fg.light_green), doodads.length);
 
-        int NUM_ROOMS = 1000;
-        
-        for (int i = 0; i < NUM_ROOMS; ++i)
+        foreach(i; 0..10000)
         {
-            int width, height, depth;
-
-            switch (dice(rng, 70, 15, 5))
-            {
-                // small room/corridor
-                case 0:
-                    width = randInt(rng, 5, 8);
-                    height = randInt(rng, 5, 8);
-                    depth = 3 + dice(rng, 50, 25, 20, 10);
-                    break;
-
-                // medium room
-                case 1:
-                    width = randInt(rng, 14, 30);
-                    height = randInt(rng, 14, 30);
-                    depth = randInt(rng, 5, 20);
-                    break;
-
-                // huge room
-                case 2:
-                    width = randInt(rng, 20, 50);
-                    height = randInt(rng, 20, 50);
-                    depth = randInt(rng, 10, 40);
-                    break;
-                default:
-                    assert(0);
-            }
-
-            int x = randInt(rng, 1, 511 - width);
-            int y = randInt(rng, 1, 511 - height);
-            int z = randInt(rng, 1, 62 - depth);
-
-            vec3i position = vec3i(x,y,z);
-            vec3i dimension = vec3i(width, height, depth);
-            box3i candidate = box3i(position, position + dimension);
-            bool intersectsOther = false;
-            for (int j = 0; j < i; ++j)
-            {
-                if (rooms[j].box().intersects(candidate))
-                    intersectsOther = true;
-                break;                
-            }
-            if (!intersectsOther)
-                rooms ~= new Room(position, dimension);
+            vec3i position = vec3i( randInt(rng, 10, 500), randInt(rng, 10, 500), randInt(rng, 1, 50));
+            objects ~= new DoodadInstance(doodads[randInt(rng, 0, 2)], position, randInt(rng, 0, 4));
         }
-        cwritefln( color("*** Created %s rooms", fg.light_green), rooms.length);
     }
 
 
     void render(ref RNG rng, AOSMap map)
-    {
-        map.fill(map.wholeWorld, Block(149, 193, 21));
+    {        
+        //map.fill(map.wholeWorld, Block(149, 193, 21));
+
+        map.clearBox(map.worldBox());
         for (int i = 0; i < 512; ++i)
             for (int j = 0; j < 512; ++j)
-                for (int k = 61; k < 62; ++k)
+                for (int k = 0; k < 1; ++k)
                 {
-                    //if (k == 0 && randInt(rng, 0, 1))
-                    //    map.block(i,j,k) = Block(149, 193, 21);
-                    //else
-                        map.block(i,j,k).empty();
+                    map.block(i,j,k) = Block(149, 193, 21);
                 }
-        //map.clearBox(map.worldBox());
-
-        // make rooms
         
-
-        foreach(room; rooms)
-            room.render(rng, map);
+        
+        foreach(di; objects)
+            di.render(map);        
     }
 
 }
